39aa0fe9a38c559ffd127ac1d41e90309ffce3b2
AlexLitt <alexlitvinovskiy@gmail.com>
2019-06-25 07:07:32 +0200

    css components

---
+++ src/components/Profile.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Profile = () => {
+  return(
+    <div className="content">
+    <img src="https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="" className="main__img"/>
+    <div className="main__content">
+      <img src="#" alt=""/>
+      <div className="main__info">
+        <div>Dmitry K.</div>
+        <div>Date of Birth: 2 january</div>
+        <div>City: Minsk</div> 
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default Profile;
